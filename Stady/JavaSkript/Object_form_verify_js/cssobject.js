5fb82b94b7b2f64d2cca438ba8edf677d3dfb78b
Viacheslav Borisov <borisov.v.1972@gmail.com>
2022-06-20 20:19:11 +0300

    Чет не сохраняет (

--- Stady/JavaSkript/Object_form_verify_js/cssobject.js
+++ Stady/JavaSkript/Object_form_verify_js/cssobject.js
@@ -6,6 +6,7 @@
 // по значению "селектор"
 // В нашем примере селектор: id="figure"
 const number = prompt('Введите количество вершин: ')
+
 class Component {
     constructor(selector) {
         this.$el = document.getElementById(selector)
@@ -19,17 +20,13 @@ class Component {
     }
 }
 
+	
 class Form extends Component {
     constructor(options) {
         super(options.selector)
-        alert(`${options.selector}`)
         this.$el.style.height = this.$el.style.width = options.size + 'px'
-        alert(`${this.$el.style.height}`)
-        alert(`${this.$el.style.width}`)        
         this.$el.style.background = options.color
-        alert(`${this.$el.style.background}`)
         const type = typeof options.pickNumber
-        alert(`${options.pickNumber}, ${type}`) 
         switch (options.pickNumber * 1) {
 // Circle            
             case 0:
@@ -37,15 +34,19 @@ class Form extends Component {
             break
 // Point
             case 1:
-                this.$el.style.height = this.$el.style.width = '10px'
+                this.$el.style.height = this.$el.style.width = '6px'
+                this.$el.style.borderRadius = '50%'    
             break
 // Otrezok
             case 2:
-                this.$el.style.height = '10px'
+                this.$el.style.height = '6px'
             break
 // Triangle
             case 3:
-            
+                this.$el.style.height = this.$el.style.width = '0px' 
+                this.$el.style.border-left  = '50px solid transparent'  
+                this.$el.style.border-right  = '50px solid transparent'
+                this.$el.style.border-bottom  = '100px solid red'
             break
 // Squere
             case 4:
@@ -58,44 +59,9 @@ class Form extends Component {
     }
 }
 
-alert('Neare object')
-
 const figure  = new Form ({
     selector: '#fig',
     size: 150,
     pickNumber: number,
     color: 'blue'
 })
-
-
-
-// class Color {
-//     constructor(color) {
-//     }
-
-//     red() {
-//         this.$el.style.color = 'red'
-//     }
-//     orange() {
-//         this.$el.style.color = 'orange'
-//     }
-//     yellow() {
-//         this.$el.style.color = 'yellow'
-//     }
-//     blue() {
-//         this.$el.style.color = 'blue'
-//     }
-//     purple() {
-//         this.$el.style.color = 'purple'
-//     }
-// }
-
-// class Size {
-//     constructor(size) {
-
-//     }
-// }
-
-
-
-
